6a4193450f59c8fddac278b6b31d3a6edb096a8b
prabas610 <33059843+prabas610@users.noreply.github.com>
2018-03-12 14:18:05 +0530

    preloader

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+jQuery(function ($) {
+
+    'use strict';
+
+    /* ---------------------------------------------- /*
+     * Preloader
+    /* ---------------------------------------------- */
+
+    $(window).ready(function() {
+        $('#pre-status').fadeOut();
+        $('#tt-preloader').delay(350).fadeOut('slow');
+    });
+});
